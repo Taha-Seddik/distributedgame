03aa8e9197558e8667741fb945c658eda114c231
YiYu.Tsinghua <YiYu.Tsinghua@d810aff8-a02a-11de-9cf1-ad53e26fafa2>
2009-09-19 07:57:11 +0000

    Original Version

---
+++  distributedgame/Distributed System/src/Hello.java
@@ -0,0 +1,14 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Hello extends Remote {
+
+	// int throwInt() throws RemoteException;
+
+	// String sayHello() throws RemoteException;
+	
+	// void joinGame() throws RemoteException;
+	
+	// void move(int id, int direction) throws RemoteException; 
+	
+}
