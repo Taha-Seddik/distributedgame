aec3c71b02220d82e177921eec55fdd0535eb0e2
YiYu.Tsinghua <YiYu.Tsinghua@d810aff8-a02a-11de-9cf1-ad53e26fafa2>
2009-09-19 07:57:11 +0000

    Original Version

---
+++ trunk/ distributedgame/Distributed System/src/Maze.java
@@ -0,0 +1,8 @@
+/*
+ * This class is used to describe maze.
+ * A maze is described as an N*N matrix, and there are M treasures there.
+ * Every location with or without treasures is marked as n (0<=n<=M)
+ */
+public class Maze {
+
+}
